27a1a4dc11d3afc9249e15c9a5e16a91ab050c0d
Anvitha <anvithakugranibsc24@rvu.edu.in>
2026-03-16 16:31:22 +0530

    commit

---
+++ practice1.py
@@ -0,0 +1,13 @@
+def transaction(num1, num2):
+    total = num1 + num2
+    difference = num1 - num2
+
+    print("Total Combined Value:", total)
+    print("Difference:", difference)
+
+
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+transaction(num1, num2)
